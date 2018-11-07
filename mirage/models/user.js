980ee729d334810d079ce1f0778514b3b996d7b5
Victor Guadarrama <victor.gm@eucledian.com>
2018-11-07 16:25:31 -0600

    Added mirage Bookmark model

--- mirage/models/user.js
+++ mirage/models/user.js
@@ -1,6 +1,5 @@
-//import { Model, hasMany } from 'ember-cli-mirage';
-import { Model } from 'ember-cli-mirage';
+import { Model, hasMany } from 'ember-cli-mirage';
 
 export default Model.extend({
-  //bookmarks: hasMany()
+  bookmarks: hasMany()
 });
